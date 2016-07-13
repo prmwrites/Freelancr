a4b62c846c0f646d9fdcb93209eadfd3f8df9f6e
Patrick Morris <prmwrites@gmail.com>
2016-07-13 18:33:56 -0400

    Connected fragment manager to InvoiceFragment

--- app/src/main/java/com/grandroid/android/freelancr/InvoiceFragment.java
+++ app/src/main/java/com/grandroid/android/freelancr/InvoiceFragment.java
@@ -1,9 +1,13 @@
 package com.grandroid.android.freelancr;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 /**
  * Created by Patrick on 7/13/2016.
@@ -11,6 +15,7 @@ import android.view.ViewGroup;
 public class InvoiceFragment extends android.support.v4.app.Fragment {
 
     private Invoice mInvoice;
+    private EditText mCustomerField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,24 @@ public class InvoiceFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_invoice, container, false);
+
+        mCustomerField = (EditText) v.findViewById(R.id.customer_name);
+        mCustomerField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mInvoice.setCustomer(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // This one too
+            }
+        });
         return v;
     }
 }
