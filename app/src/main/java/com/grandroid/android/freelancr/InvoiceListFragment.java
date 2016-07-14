3b8b35bbc16b7fff06ab0b03c452a8f358b42191
Patrick Morris <prmwrites@gmail.com>
2016-07-14 15:13:21 -0400

    Setup RecyclerView

--- app/src/main/java/com/grandroid/android/freelancr/InvoiceListFragment.java
+++ app/src/main/java/com/grandroid/android/freelancr/InvoiceListFragment.java
@@ -1,10 +1,26 @@
 package com.grandroid.android.freelancr;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Patrick on 7/14/2016.
  */
 public class InvoiceListFragment extends Fragment {
-    // Nothing yet
+    private RecyclerView mInvoiceRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_invoice_list, container, false);
+
+        mInvoiceRecyclerView = (RecyclerView) view.findViewById(R.id.invoice_recycler_view);
+        mInvoiceRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 }
