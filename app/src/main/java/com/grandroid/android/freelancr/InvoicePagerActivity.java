6651dd8ddc49ad9a531684f8bc4131e045c35a7f
Patrick Morris <prmwrites@gmail.com>
2016-07-17 12:34:10 -0400

    InvoicePagerActivity created, ViewPager view created

---
+++ app/src/main/java/com/grandroid/android/freelancr/InvoicePagerActivity.java
@@ -0,0 +1,15 @@
+package com.grandroid.android.freelancr;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Patrick on 7/17/2016.
+ */
+public class InvoicePagerActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice_pager);
+    }
+}
