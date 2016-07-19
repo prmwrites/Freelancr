6c01214df244f02bbf9816ec5211a7348a9ba508
Patrick Morris <prmwrites@gmail.com>
2016-07-19 15:05:19 -0400

    Wired up DatePicker

--- app/src/main/java/com/grandroid/android/freelancr/DatePickerFragment.java
+++ app/src/main/java/com/grandroid/android/freelancr/DatePickerFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by Patrick on 7/19/2016.
@@ -11,6 +13,7 @@ import android.support.v7.app.AlertDialog;
 public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity()).setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null).create();
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+        return new AlertDialog.Builder(getActivity()).setView(v).setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null).create();
     }
 }
